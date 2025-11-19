5b1bd27b4fdbeb0c8fc709d4f1e881215342f9ed
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2025-11-19 18:11:37 +0000

    Adding a basic program to compare two numbers taken as input

---
+++ Conditionals/NumbersComparison.py
@@ -0,0 +1,8 @@
+a = int(input("Enter 1st number: "))
+b = int(input("Enter 2nd number: "))
+if(a>b):
+    print(a,"is greater than",b)
+elif(a<b):
+    print(a,"is less than",b)
+else:
+    print(a,"and",b,"are equal")
