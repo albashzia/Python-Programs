fd60882d3990f0069ec4a3e6e0a3a4af6b4ebd41
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2025-12-02 21:54:36 +0500

    Demonstrate nested for loops for two separate lists

---
+++ Loops/nested_list_iteration.py
@@ -0,0 +1,6 @@
+subjects = ['Maths','English','Urdu']
+grades = ['A','B']
+for subject in subjects:
+    print("The name of the subject is:",subject)
+    for grade in grades:
+        print("The name of the grade is:", grade)
