34b091b1d32ae4291e4ab1960788094718cfdd9c
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2026-01-05 18:48:41 +0500

    Update VowelCounter.py

--- Small_Projects/VowelCounter.py
+++ Small_Projects/VowelCounter.py
@@ -1,8 +1,16 @@
+# taking input from user
 Str = input("Enter a string: ")
+# converting the string to lowercase
 Str = Str.lower()
+# storing length of string in l variable
 l = len(Str)
+# counter to count the number of variables
 count = 0
+# looping over the entire string
 for i in range(l):
+    # checking for presence of vowels in the string
     if Str[i] == 'a' or Str[i] == 'e' or Str[i] == 'i' or Str[i] == 'o' or Str[i] == 'u':
+        # incrementing coun tvariable if vowel is found
         count = count + 1
+# printing the number of vowels at end after looping over the whole string
 print("The number of vowels present is given string is ",count)
