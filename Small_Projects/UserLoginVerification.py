bb9ed49bae3f948d604b7abaf73a4df5c0643b3d
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2026-01-05 18:45:55 +0500

    Update UserLoginVerification.py

--- Small_Projects/UserLoginVerification.py
+++ Small_Projects/UserLoginVerification.py
@@ -1,11 +1,18 @@
+# setting default user_name, password and otp
 user_name = "user"
 password = int(87654321)
 otp = int(1234)
+
+# taking username, password and otp from the user
 entered_name = input("Enter Your user name : ")
 entered_password = int(input("Enter your password: "))
 entered_otp = int(input("Enter the OTP: "))
+
+# comparing entered inputs with default values using conditionals using user_name
 if(entered_name == user_name):
+    # comparing entered inputs with default values using conditionals using password
     if(entered_password == password):
+        # comparing entered inputs with default values using conditionals using otp
         if(entered_otp == otp):
             print("Login Succeddful")
         else:
