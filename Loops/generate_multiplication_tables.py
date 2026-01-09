7a00d3139f0924e85c2af3ded1b0dcacf6d24ae5
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2026-01-10 00:43:01 +0500

    Improve readability of multiplication table code
    Refactor multiplication table generation to include comments and clarify loop increments.

--- Loops/generate_multiplication_tables.py
+++ Loops/generate_multiplication_tables.py
@@ -1,6 +1,8 @@
+# looping over a range from 1 to 9
 for i in range(1,9):
-    print("Table of",i)
+    print("Table of",i) # printing a statement before printing each table
+    # looping over a range from 1 to 10
     for k in range(1,11):
-        print(i," x ",k," = ",i*k)
-    k = k + 1
-    i = i + 1
+        print(i," x ",k," = ",i*k) # printing the actual statements
+    k = k + 1 # incrementing k 
+    i = i + 1 # incrementing i
