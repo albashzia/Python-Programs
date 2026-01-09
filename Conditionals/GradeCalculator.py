fb3088dca5448cf4f27915cdae598864e18baf6a
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2026-01-10 00:56:37 +0500

    Enhance score input validation and comments
    Added input validation for score exceeding 100 and improved comments.

--- Conditionals/GradeCalculator.py
+++ Conditionals/GradeCalculator.py
@@ -1,15 +1,29 @@
-score = int(input("Enter your score: "))
-if score > 100:
+score = int(input("Enter your score: ")) # taking grade as an input from the user
+
+# tackling invalid entered score i.e greater than 100
+if score > 100: 
     print("Invalid Score")
+
+# printing A+ if score is greater than or equal to 90
 elif score >= 90:
     print("A+")
+
+# printing A if score is greater than or equal to 85
 elif score >= 85:
     print("A")
+
+# printing B if score is greater than or equal to 75
 elif score >= 75:
     print("B")
+
+# printing C if score is greater than or equal to 65
 elif score >= 65:
     print("C")
+
+# printing D if score is greater than or equal to 50
 elif score >= 50:
     print("D")
+    
+# printing F if score is less than 50
 elif score < 50:
     print("F")
