405948111de3f4bf592ef0c026a315f29557b566
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2026-01-10 00:49:16 +0500

    Comment even number printing program
    Refactor the loop to print even numbers from 1 to 100.

--- Loops/print_even_numbers_1_to_100.py
+++ Loops/print_even_numbers_1_to_100.py
@@ -1,5 +1,6 @@
+# looping over a range of 1 to 100
 for i in range(1,101):
-    if(i%2==0):
-        print(i , end = " ")
-    i = i + 1
-print()
+    if(i%2==0): # checking if a number is even or not
+        print(i , end = " ") # printing of a number is even 
+    i = i + 1 # incrementing i 
+print() # moving to next line
