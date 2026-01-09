32231218f8aaf1fb8b62a122e7f9da28c6113424
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2026-01-10 00:45:12 +0500

    Add comments to iterate_list_elements.py

--- Loops/iterate_list_elements.py
+++ Loops/iterate_list_elements.py
@@ -1,3 +1,3 @@
-subjects = ['Maths','English','Urdu']
-for subject in subjects:
-    print("The name of the subject is:",subject)
+subjects = ['Maths','English','Urdu'] # declaring a list 
+for subject in subjects: # looping over all list entries
+    print("The name of the subject is:",subject) # printing the elements of list 1-by-1
