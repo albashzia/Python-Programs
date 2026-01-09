07eb16b9e723a0b0cac6580a69729c25e9387edc
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2026-01-09 06:46:52 +0500

    Update LeapYearChecker.py

--- Small_Projects/LeapYearChecker.py
+++ Small_Projects/LeapYearChecker.py
@@ -1,5 +1,7 @@
-year = int(input("Enter the year: "))
+year = int(input("Enter the year: ")) # taking input from the user
+ 
 if(year%4==0 and year%100!=0) or (year%400==0):
     print("Leap Year")
+
 else:
     print("Not a Leap Year")
