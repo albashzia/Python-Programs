4183b3837e2a924ffea59c75f5a4249f04c2758e
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2026-01-10 00:50:56 +0500

    Enhance print_numbers_to_n.py with comments
    Added comments for clarity and adjusted print logic.

--- Loops/print_numbers_to_n.py
+++ Loops/print_numbers_to_n.py
@@ -1,4 +1,5 @@
-n = int(input("Enter the number n : "))
+n = int(input("Enter the number n : ")) # taking an integer number as input from the user
+# looping over a range from 1 to inputted number m 
 for i in range(1,n):
-    print(i)
-    i = i + 1
+    print(i) # printing the number
+    i = i + 1 # incrementing i 
