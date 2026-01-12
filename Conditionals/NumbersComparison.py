51d9fc5a5c6360ae90380809792079f763d6730f
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2026-01-12 20:05:14 +0500

    Refactor input and comparison logic in NumbersComparison.py

--- Conditionals/NumbersComparison.py
+++ Conditionals/NumbersComparison.py
@@ -1,8 +1,8 @@
-a = int(input("Enter 1st number: "))
-b = int(input("Enter 2nd number: "))
-if(a>b):
+a = int(input("Enter 1st number: ")) # taking first number as input from the user
+b = int(input("Enter 2nd number: ")) # taking second number as input from user
+if(a>b): # checking if a is greater than b 
     print(a,"is greater than",b)
-elif(a<b):
+elif(a<b): # checking if a is less than b
     print(a,"is less than",b)
-else:
+else: # handling if both a and b are equal 
     print(a,"and",b,"are equal")
