2be89d7ce85125f9ab887476fdd47820c1e62e86
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2026-01-12 20:23:31 +0500

    Enhance TicketPricingSystem with comments
    Added comments to clarify the logic for ticket pricing based on age and student status.

--- Conditionals/TicketPricingSystem.py
+++ Conditionals/TicketPricingSystem.py
@@ -1,18 +1,20 @@
-age = int(input("Enter the age: "))
-status = input("Is the customer a student?(Y/N) : ").strip().lower()
+age = int(input("Enter the age: ")) # taking age as an input from the user 
+status = input("Is the customer a student?(Y/N) : ").strip().lower() # taking customer's status from the user 
 
+# handling condistion if age is less than 18
 if age < 18:
-    if status == "y":
+    if status == "y": # handling if the customer is a student
         print("Ticket Price = 150")
-    elif status == "n":
+    elif status == "n": # handling if the customer is not a student
         print("Ticket price = 200")
-    else:
+    else: # handling invalid input entered
         print("Invalid Status")
 
+# handling if age is equal to or greater than 18
 elif age >=18:
-    if status == "y":
+    if status == "y": # handling if the customer is a student
         print("Ticket Price = 250")
-    elif status == "n":
+    elif status == "n": # handling if the customer is not a student
         print("Ticket price = 300")
-    else:
+    else: # handling invalid input entered
         print("Invalid Status")
