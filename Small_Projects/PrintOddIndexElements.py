4d14faadb518c5828b4e0e5a70a8589fc40b8673
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2026-01-15 23:40:55 +0500

    Update PrintOddIndexElements.py

--- Small_Projects/PrintOddIndexElements.py
+++ Small_Projects/PrintOddIndexElements.py
@@ -1,7 +1,10 @@
+# taking input from the user 
 str = input("Enter a string : ")
+# storing length of input string in a variable 
 l = len(str)
+# decalring an empty string
 new_str = ""
 for i in range(l):
-    if(i%2!=0):
-        new_str = new_str+str[i]
-print(new_str)
+    if(i%2!=0): # filtering odd index element 
+        new_str = new_str+str[i] # adding the elements at odd index to new_str
+print(new_str) # printing new_str 
