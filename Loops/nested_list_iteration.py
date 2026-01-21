a8ad1d315296727ca81aacf2d6fece368af349db
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2026-01-21 19:19:07 +0500

    Update nested_list_iteration.py

--- Loops/nested_list_iteration.py
+++ Loops/nested_list_iteration.py
@@ -1,6 +1,11 @@
+# making a list of subjects
 subjects = ['Maths','English','Urdu']
+# making a list of grades
 grades = ['A','B']
+
+# running for each loop on subjects
 for subject in subjects:
     print("The name of the subject is:",subject)
+    # running for each loop for grades
     for grade in grades:
         print("The name of the grade is:", grade)
