68531339ca64c67972714099f5441295b41fdd6b
albashzia <ahmedalbashzia@gmail.com>
2026-06-06 20:04:56 +0500

    Initializing a contacts dictionary in contact_book.py

---
+++ Small_Projects/contact_book.py
@@ -0,0 +1,17 @@
+contacts = {
+    "Muhammad Ali": "+92-300-1234567",
+    "Ayesha Khan": "+92-321-7654321",
+    "Zainab Ahmed": "+92-333-9876543",
+    "Bilal Shah": "+92-345-1122334",
+    "Fatima Malik": "+92-312-5566778",
+    "Hamza Hussain": "+92-301-4433221",
+    "Yousaf": "+92-322-8899001",
+    "Omer Farooq": "+92-334-2233445",
+    "Amna": "+92-346-6677889",
+    "Usman Raza": "+92-313-9988776",
+    "Mariam Javed": "+92-302-3344556",
+    "Asad Mahmood": "+92-323-5544332",
+    "Hira Iqbal": "+92-335-7788990",
+    "Zeeshan Tariq": "+92-347-1133557",
+    "Rehman": "+92-314-2244668"
+}
