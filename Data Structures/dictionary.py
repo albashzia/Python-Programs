ff2227c77e9de1726ccea9cde23b2fe98eb48777
albashzia <ahmedalbashzia@gmail.com>
2026-06-06 17:26:50 +0500

    Documenting the dictionary.py file using comments to explain the code and improve readability

--- Data Structures/dictionary.py
+++ Data Structures/dictionary.py
@@ -1,23 +1,35 @@
+#defining a dictionary -- Structure = key -> value
 book = {
     "title" : "Meditations",
     "author" : "Marcus Aurelius",
     "pages" : 120
 }
 
+#printing the values using keys
 print(book["title"])
 print(book["author"])
 print(book["pages"])
 
+#modifying the value of a key
 book["pages"] = 90
 
+#printing the whole dictionary values
 print(book)
 
+#deleting a key and its value
 book.pop("pages")
 
+#printing the dictionary
 print(book)
 
+#printing the keys of the dictionary
 print(book.keys())
+
+#printing the values of the dictionary
 print(book.values())
+
+#printing the items of the dictionary
 print(book.items())
 
+#accessing a non-existent key that returns "None" without crashing the code
 print(book.get("pages"))
