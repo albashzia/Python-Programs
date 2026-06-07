67a075df22803c567b1bf003bae4a6b57143e2ac
albashzia <ahmedalbashzia@gmail.com>
2026-06-07 19:57:55 +0500

    Using insert function to insert objects at the specific locations inside the list

--- Data Structures/list.py
+++ Data Structures/list.py
@@ -4,7 +4,8 @@ words_list = ["Apple","Book","Shoes","Pen","Laptop","Fan"]
 numbers_list.append(3)
 words_list.append("Study")
 
-
+numbers_list.insert(3,9)
+words_list.insert(2,"Lamp")
 
 for i in numbers_list:
     print(i)
