b49709651ac2f03dec648d3260c2efd6fe47da25
albashzia <ahmedalbashzia@gmail.com>
2026-06-07 19:56:43 +0500

    Using for-each loops to print all the elements of the lists

--- Data Structures/list.py
+++ Data Structures/list.py
@@ -1,5 +1,13 @@
-numbers_list = [4,2,1,3,5,6,8,2,4,8,9,3,4,6,8,2,1,0]
+numbers_list = [4,2,1,4,8,9,3,4,6,8,2,1,0]
 words_list = ["Apple","Book","Shoes","Pen","Laptop","Fan"]
 
 numbers_list.append(3)
 words_list.append("Study")
+
+
+
+for i in numbers_list:
+    print(i)
+
+for j in words_list:
+    print(j)
