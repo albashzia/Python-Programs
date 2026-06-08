820da5bdd2b8654b9ac85021302ddbc13e2b54e2
albashzia <ahmedalbashzia@gmail.com>
2026-06-08 20:48:00 +0500

    Defining a function to display menu in the program

--- Small_Projects/shopping_list_manager.py
+++ Small_Projects/shopping_list_manager.py
@@ -1,1 +1,12 @@
 shopping_list = []
+
+def menu():
+    print(
+        "\n1. Add Item"
+        "\n2. Remove Item"
+        "\n3. View Shopping List"
+        "\n4. Search Item"
+        "\n5. Count Items"
+        "\n6. Clear List"
+        "\n7. Exit"
+    )
