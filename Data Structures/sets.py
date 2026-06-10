a253b8744be2fefd90199257c403ac917d8ec188
albashzia <ahmedalbashzia@gmail.com>
2026-06-10 22:03:22 +0500

    Creating another number set inside sets.py file

--- Data Structures/sets.py
+++ Data Structures/sets.py
@@ -1,4 +1,5 @@
-number_set = {1,2,3,4,5,6,4,3,2,4,2}
+number_set1 = {1,2,3,4,5,6,4,3,2,4,2}
+number_set2 = {10,11,12,23,45,67,3,2,5,6,2,6,1,3,6}
 
 words_set = {"book","bag","shoes","laptop","book"}
 
@@ -6,4 +7,4 @@ words_set.add("keys")
 
 words_set.remove("bag")
 
-number_set.pop()
+number_set1.pop()
